be3e72755a76559a5e025a6d4ccd016212e37130
Cong Nguyen <rampadc@users.noreply.github.com>
2022-08-16 23:09:29 +1000

    added managed cluster set env variable

--- main.sh
+++ main.sh
@@ -11,11 +11,20 @@ if [ -z "${CLUSTER_CLAIM_NAMESPACE}" ]; then
     echo "Please provide CLUSTER_CLAIM_NAMESPACE as an environment variable"
     exit
 fi
+if [ -z "${MANAGED_CLUSTER_SET}" ]; then
+    echo "Please provide MANAGED_CLUSTER_SET as an environment variable, if none, use `default`"
+    exit
+fi
+
 
 # https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.5/html/clusters/managing-your-clusters#cli-prerequisites
 echo "Preparing cluster for imports"
+echo "-- Get claimed cluster name"
 export CLUSTER_NAME="$(oc get clusterclaims.hive.openshift.io/$CLUSTER_CLAIM_NAME -o=jsonpath='{.spec.namespace}' -n $CLUSTER_CLAIM_NAMESPACE)"
 oc label namespace ${CLUSTER_NAME} cluster.open-cluster-management.io/managedCluster=${CLUSTER_NAME} --overwrite=true
+echo "-- Create a new managed cluster CR"
+echo "-- Get the managed cluster set for cluster pool (if applicable)"
+export CLUSTER_POOL_NAME="$(oc get clusterclaims.hive.openshift.io/$CLUSTER_CLAIM_NAME -o=jsonpath='{.spec.clusterPoolName}' -n $CLUSTER_CLAIM_NAMESPACE)"
 envsubst < ./managed-cluster.yaml | oc apply -f -
 
 echo "Importing the cluster with the auto import secret"
